283eef04818cd779f8994fe18c2227fa312257fa
Darko Mišov <darko.misov@gmail.com>
2025-03-21 12:19:34 +0100

    v0.1.5 export FieldBase

--- src/index-individual.ts
+++ src/index-individual.ts
@@ -5,5 +5,6 @@ export * from './actions';
 export * from './action';
 export * from './field';
 export * from './field.interface';
+export * from './field-base';
 export * from './group';
 export * from './list';
